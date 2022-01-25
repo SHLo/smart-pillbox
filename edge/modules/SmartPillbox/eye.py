9b5bc734ecc179e645675c2f8fae63e3f8aae889
shlo <shlo.sam@gmail.com>
2022-01-25 10:58:38 -0800

    2 motors

--- edge/modules/SmartPillbox/eye.py
+++ edge/modules/SmartPillbox/eye.py
@@ -2,6 +2,7 @@ import cv2
 import json
 from azure.iot.device import Message
 import users
+import asyncio
 
 face_cascade = cv2.CascadeClassifier(
     './config/haarcascade_frontalface_default.xml')
@@ -19,7 +20,8 @@ async def snap(client):
             print(f'user detected! {user}')
             text = f'hello {user["first_name"]}, please take your pills in slot {user["slot"]}'
             await client.send_message_to_output(Message(json.dumps({'text': text}), content_encoding='utf-8', content_type='application/json'), 'mouth')
-            await client.send_message_to_output(Message(json.dumps({'motor': 'a', 'rounds': 2, 'clock_wise': True}), content_encoding='utf-8', content_type='application/json'), 'tray')
+            await client.send_message_to_output(Message(json.dumps({'motor': user['slot'], 'rounds': 2, 'clock_wise': True}), content_encoding='utf-8', content_type='application/json'), 'tray')
+            await asyncio.sleep(5)
 
     cap.release()
 
