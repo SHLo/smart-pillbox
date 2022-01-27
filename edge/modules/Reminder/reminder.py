549055d0302cb1c3d87bd16368b447cfe4b4cac0
shlo <shlo.sam@gmail.com>
2022-01-26 19:39:42 -0800

    remind on DB

--- edge/modules/Reminder/reminder.py
+++ edge/modules/Reminder/reminder.py
@@ -4,29 +4,42 @@ import users
 import json
 import activities
 from azure.iot.device import Message
-from azure.iot.device import IoTHubModuleClient
 
-import datetime
-
-module_client = IoTHubModuleClient.create_from_edge_environment()
-module_client.connect()
+from datetime import timedelta
 
 
-def remind(user):
-    text = f'{user["first_name"]}, it is your time to take pills in tray {user["slot"]}'
-
-    module_client.send_message_to_output(Message(json.dumps(
-        {'text': text}), content_encoding='utf-8', content_type='application/json'), 'mouth')
+def remind(user, client):
+    speak(user, client)
 
     activities.create(user)
 
+    repeat_interval = 1
 
-def set_schedule():
+    schedule.every(repeat_interval).minutes.until(timedelta(minutes=repeat_interval * 3.5)).do(
+        repeat_remind, user=user, client=client)
+
+
+def speak(user, client):
+    text = f'{user["first_name"]}, it is your time to take pills in tray {user["slot"]}'
+
+    client.send_message_to_output(Message(json.dumps(
+        {'text': text}), content_encoding='utf-8', content_type='application/json'), 'mouth')
+
+
+def repeat_remind(user, client):
+    if activities.is_completed(user):
+        return schedule.CancelJob
+
+    speak(user, client)
+
+
+def reset_schedule(client):
+    schedule.clear()
     users.update_data()
 
     for user in users.get_data():
         for t in user['schedule']['time']:
-            schedule.every().day.at(t).do(remind, user=user)
+            schedule.every().day.at(t).do(remind, user=user, client=client)
 
 
 async def loop():
