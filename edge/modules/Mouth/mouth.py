b7fc9f32ba3e8a5e254e4f3ad2731f1be9fb476a
shlo <shlo.sam@gmail.com>
2022-02-09 21:04:45 -0800

    format

--- edge/modules/Mouth/mouth.py
+++ edge/modules/Mouth/mouth.py
@@ -6,7 +6,7 @@ import simpleaudio as sa
 
 
 os.makedirs('./audio', exist_ok=True)
-polly = Session().client("polly")
+polly = Session().client('polly')
 
 
 def encode_filename(text):
@@ -27,15 +27,15 @@ def speak(text):
     filepath_wav = os.path.join('./audio/', f'{filename}.wav')
 
     if not os.path.exists(filepath_wav):
-        response = polly.synthesize_speech(Text=text, OutputFormat="mp3",
-                                           VoiceId="Joanna")
+        response = polly.synthesize_speech(Text=text, OutputFormat='mp3',
+                                           VoiceId='Joanna')
 
-        if "AudioStream" not in response:
+        if 'AudioStream' not in response:
             return
 
-        with closing(response["AudioStream"]) as stream:
+        with closing(response['AudioStream']) as stream:
             filepath_mp3 = os.path.join('./audio/', f'{filename}.mp3')
-            with open(filepath_mp3, "wb") as file:
+            with open(filepath_mp3, 'wb') as file:
                 file.write(stream.read())
 
         mp3 = AudioSegment.from_mp3(filepath_mp3)
